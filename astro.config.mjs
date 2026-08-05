36f15a7af3cf1e8a05806d4a7bf895fbed403131
alangotzo <naisykes6@gmail.com>
2026-08-05 19:42:45 -0300

    try 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 site: 'https://alangotzo.github.io',
-base: '/frich-landing',
-build: {
-    assetsPrefix: '/frich-landing',  
-  },
 });
