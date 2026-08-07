179dfad204663425ef6de6d3607663b37f243637
alangotzo <naisykes6@gmail.com>
2026-08-07 15:53:05 -0300

    Fix hero background image resolution

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 site: 'https://alangotzo.github.io',
-base: '/frich-page'
+base: '/frich-page',
 });
